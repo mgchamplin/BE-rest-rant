df329b65c2ecfa666df84cc2ac57dda6c04614e0
Michael Champlin <mchamplin@sbcglobal.net>
2022-03-23 15:46:46 -0700

    Switch route downwards

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const app = express()
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-app.use('/places',      require('./controllers/show-places'))
 app.use('/addplace',    require('./controllers/add-place'))
 app.use('/editdetails', require('./controllers/edit-details'))
 app.use('/showdetails', require('./controllers/show-details'))
-
+app.use('/places',      require('./controllers/show-places'))
 
 app.get('/', (req, res) => {res.render("home")})
 app.get('*', (req, res) => {res.render('error404')})
