adf7e605fe65dd986a6f119d88fc8f91c92b46fd
Michael Champlin <mchamplin@sbcglobal.net>
2022-03-23 16:28:57 -0700

    Added styles (css) to start

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/addplace',    require('./controllers/add-place'))
 app.use('/editdetails', require('./controllers/edit-details'))
 app.use('/showdetails', require('./controllers/show-details'))
@@ -14,3 +19,9 @@ app.get('/', (req, res) => {res.render("home")})
 app.get('*', (req, res) => {res.render('error404')})
 
 app.listen(process.env.PORT) 
+
+
+
+
+
+
