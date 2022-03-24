4c850bca8d7ae779cdb6972ae2f61af72953d15e
Michael Champlin <mchamplin@sbcglobal.net>
2022-03-23 20:50:43 -0700

    Minor changes...

--- views/home.jsx
+++ views/home.jsx
@@ -5,12 +5,12 @@ function home () {
     return (
       <Def>
           <main>
-                <h1>HOME</h1>
+                <h1>REST-rant</h1>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
                 <div>
-                  <img align="middle" src="/images/Pizza.jpg" alt="Juicy Pizza" />
+                  <img src="/images/Pizza.jpg" width={800} alt="Juicy Pizza" />
                 </div>
             </main>
       </Def>
