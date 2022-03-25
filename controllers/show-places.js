92790f37f9ee6273caf6efa28644226fc2bce050
Michael Champlin <mchamplin@sbcglobal.net>
2022-03-24 21:47:59 -0700

    Success in adding a new restaurant (/places/new

--- controllers/show-places.js
+++ controllers/show-places.js
@@ -1,29 +1,53 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 // GET /places
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.post('/', (req, res) => {
-  res.send('POST places')
+router.get('/', (req, res) => {
+  res.render('places/index',{places}) 
 })
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: "Angelo's",
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Italian Cuisine',
-        pic: '/images/italian.webp'
-      }, {
-        name: "Ziba's",
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Mediterranean Cuisine',
-        pic: '/images/ziba-special.png'
-      }]
-    res.render('places/index',{places}) 
-  })
+router.post('/', (req, res) => {
+  console.log(places)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
 
-module.exports = router
+module.exports = router 
+
+/*
+const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+
+// More code ...
+let places = [{
+  name: "Angelo's",
+  city: 'Seattle',
+  state: 'WA',
+  cuisines: 'Italian Cuisine',
+  pic: '/images/italian.webp'
+}, {
+  name: "Ziba's",
+  city: 'Phoenix',
+  state: 'AZ',
+  cuisines: 'Mediterranean Cuisine',
+  pic: '/images/ziba-special.png'
+}]
+*/
---
+++ models/places.js
@@ -0,0 +1,15 @@
+var places = [{
+    name: "Angelo's",
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Italian Cuisine',
+    pic: '/images/italian.webp'
+    }, {
+    name: "Ziba's",
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Mediterranean Cuisine',
+    pic: '/images/ziba-special.png'
+}]
+
+module.exports = places;
